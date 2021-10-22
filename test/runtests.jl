284144c4250cc2099bffaa8ff7a5c5bec3164e5f
kozvojtex <vojtech.kozel.cz@gmail.com>
2021-10-22 18:23:23 +0200

    added some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,16 +5,29 @@ using Ecosystem
 @testset "agent_count" begin
     grass1 = Grass(1,1,5)
     grass2 = Grass(2,2,5)
-    sheep  = Animal{Sheep, Female}(3,1,1,1,1)
-    wolf   = Animal{Wolf, Male}(5,2,2,2,2)
-    world  = World([sheep,grass1,grass2,wolf])
+    sheep1  = Animal{Sheep, Female}(3,1,1,1,1)
+    sheep2  = Animal{Sheep, Male}(4,1,1,1,1)
+    wolf1   = Animal{Wolf, Male}(5,2,2,2,2)
+    wolf2   = Animal{Wolf, Female}(6,2,2,2,2)
+    mushroom = Mushroom(7, 4, 5)
+    world  = World([sheep1, sheep2, grass1, grass2, wolf1, wolf2, mushroom])
 
     @test agent_count(grass1) ≈ 0.2
-    @test agent_count(sheep) == 1
+    @test agent_count(sheep1) == 1
+    @test agent_count(mushroom) == 0.8
     @test agent_count([grass2,grass1]) ≈ 0.6
     res = agent_count(world)
-    tst = Dict(Animal{Wolf, Male}=>1, Plant{Grass}=>0.6, Animal{Sheep, Female}=>1)
+    tst = Dict(Animal{Wolf, Male}=>1, 
+               Animal{Wolf, Female}=>1, 
+               Animal{Sheep, Male}=>1,
+               Animal{Sheep, Female}=>1,
+               Plant{Grass}=>0.6,
+               Plant{Mushroom}=>0.8)
     for (k,_) in res
         @test res[k] ≈ tst[k]
     end
+    @test eats(sheep1, grass1) == true
+    @test eats(sheep1, wolf1) == false
+    @test eats(wolf1, mushroom) == false
+    @test eats(wolf1, sheep1) == true
 end
