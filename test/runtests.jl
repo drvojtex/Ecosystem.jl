b803694f374c8065c73608c95ec9b68109da7bde
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-12-18 23:07:59 +0100

    test up

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,11 +3,16 @@ using Test
 using Ecosystem
 
 
-# agent_count tests
-include("agent_count.jl")
+# animal test
+include("animal.jl")
+# plant test
+include("plant.jl")
 # every_nth tests
 include("every_nth.jl")
+# agent_count tests
+include("agent_count.jl")
 # mushroom tests
 include("mushroom.jl")
 # macros tests
 include("macros.jl")
+
