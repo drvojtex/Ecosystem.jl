8da52d9559f088d134d93e325454eed623a6d61a
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-12-18 21:08:05 +0100

    split to boundary values & behaviour parts

--- test/every_nth.jl
+++ test/every_nth.jl
@@ -1,16 +1,28 @@
 
-@testset "every_nth" begin
-    max_i = 2000
-    max_j = 2000
+@testset "every_nth: boundary values" begin
     iter = 0
     function incr_iter()
         iter += 1
     end
+    @test_throws MethodError typeof(every_nth((x)->incr_iter(), "2")) <: Function
+    @test_throws MethodError typeof(every_nth((x)->incr_iter(), [])) <: Function
+    @test_throws MethodError typeof(every_nth((x)->incr_iter(), Nothing)) <: Function
+    @test_throws MethodError typeof(every_nth((x)->incr_iter(), 1.2)) <: Function
+end
+
+@testset "every_nth: behaviour" begin
+    max_i = 10
+    max_j = 30
+    iter = 0
+    function incr_iter()
+        iter += 1
+    end
+
     for i=1:max_i
         iter = 0
-        en = every_nth(x->incr_iter(), i);
-        for j=1:max_j
-            en(j);
+        en = every_nth((x)->incr_iter(), i);
+        for _=1:max_j
+            en(Nothing);
         end
         @test iter == Int(floor(max_j/i))
     end
