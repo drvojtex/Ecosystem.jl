17d8c1f13bd5b95a1c80f0d6c2127614d85819e0
kozvojtex <vojtech.kozel.cz@gmail.com>
2022-12-20 15:31:08 +0100

    test coverage up

--- test/mushroom.jl
+++ test/mushroom.jl
@@ -1,23 +1,4 @@
 
-@testset "mushroom" begin
-    for dE=1:rand(1:10)
-        for e=1:rand(1:10)
-            for mm=1:rand(1:10)
-                for ms=1:rand(1:mm)
-                    sheep  = Animal{Sheep, Female}(1, e, dE, 1, 1)
-                    mushroom = Mushroom(2, ms, mm)
-                    @test mushroom.size == ms
-                    @test mushroom.max_size == mm
-                    world = World([sheep, mushroom])
-                    eat!(sheep, mushroom, world);
-                    @test size(world.agents[2]) == 0
-                    @test energy(world.agents[1]) == e-dE*ms
-                end
-            end
-        end
-    end    
-end
-
 @testset "mushroom: behaviour" begin
     sheep = Sheep(1, 5, 1, 1, 1)
     mushroom = Mushroom(2, 1, 2)
